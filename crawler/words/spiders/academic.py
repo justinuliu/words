7a3c499660e7deb8e19b6b42e4a0439d32b98d14
刘甲 <liujia01@chinayaan.com>
2014-08-12 09:25:03 +0800

    Now we get all pages have words.

--- crawler/words/spiders/academic.py
+++ crawler/words/spiders/academic.py
@@ -4,6 +4,13 @@ from words.items import Word
 
 
 class AcademicSpider(CrawlSpider):
-    name = 'AcademicSpider'
-    start_urls = ['http://www.oxfordlearnersdictionaries.com/wordlist/english/academic/']
-    pass
+    name = 'academic'
+    allowed_domains = ['oxfordlearnersdictionaries.com']
+    start_urls = ['http://www.oxfordlearnersdictionaries.com/wordlist/english/academic/sublist01/?page=1']
+
+    def parse(self, response):
+        for url in response.selector.xpath("//ul/li/a[contains(.//text(),'sublist')]/@href").extract():
+            yield scrapy.Request(url, callback=self.parse)
+
+        for url in response.selector.xpath('//div/span/a/@href').extract():
+            yield scrapy.Request(url, callback=self.parse)
