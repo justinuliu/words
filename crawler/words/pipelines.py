e1185e9ffc96868f14dbede323d13ff897e6b668
刘甲 <liujia01@chinayaan.com>
2014-08-14 15:52:14 +0800

    Add pipeline for storing words in future.

--- crawler/words/pipelines.py
+++ crawler/words/pipelines.py
@@ -4,8 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
 
 
 class WordsPipeline(object):
+    '''Store words into sqlite'''
+    def open_spider(self, spider):
+        '''Open connection to sqlite'''
+        print 'open spider'
+
+    def close_spider(self, spider):
+        '''Close connection to sqlite'''
+        print 'close spider'
+
     def process_item(self, item, spider):
-        return item
+        '''Store them'''
+        print 'Hello item %s' % type(item)
+        return DropItem()
--- crawler/words/settings.py
+++ crawler/words/settings.py
@@ -12,6 +12,8 @@ BOT_NAME = 'words'
 
 SPIDER_MODULES = ['words.spiders']
 NEWSPIDER_MODULE = 'words.spiders'
-
+ITEM_PIPELINES = {
+    'words.pipelines.WordsPipeline': 500,
+    }
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'words (+http://www.yourdomain.com)'
